5b9a23ad8a436c81ffab64adde56cc140479a35e
Joe Ebmeier <joe.ebmeier@firespring.com>
2018-01-31 13:38:35 -0600

    Install aws cli and add it to the path. Remove git dependency in favor of curl.

--- install.sh
+++ install.sh
@@ -94,15 +94,16 @@ if ! which aws; then
 fi
 
 tmpdir=$(mktemp -d)
-
 cd "$tmpdir"
 
-git clone -b master https://github.com/widdix/aws-ec2-ssh.git
+AWSCLI_GITHUB_VERSION=${AWSCLI_GITHUB_VERSION:-develop}
+easy_install https://github.com/aws/aws-cli/archive/${AWSCLI_GITHUB_VERSION}.tar.gz
+PATH=${PATH}:/usr/local/bin
 
-cd "$tmpdir/aws-ec2-ssh"
-
-cp authorized_keys_command.sh $AUTHORIZED_KEYS_COMMAND_FILE
-cp import_users.sh $IMPORT_USERS_SCRIPT_FILE
+EC2SSH_GITHUB_VERSION=${EC2SSH_GITHUB_VERSION:-master}
+curl -L https://github.com/widdix/aws-ec2-ssh/archive/${EC2SSH_GITHUB_VERSION}.tar.gz | tar -xzf -
+cp "./aws-ec2-ssh-${EC2SSH_GITHUB_VERSION}/authorized_keys_command.sh" $AUTHORIZED_KEYS_COMMAND_FILE
+cp "./aws-ec2-ssh-${EC2SSH_GITHUB_VERSION}/import_users.sh" $IMPORT_USERS_SCRIPT_FILE
 
 if [ "${IAM_GROUPS}" != "" ]
 then
